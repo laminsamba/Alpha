59149772c009c094a0006324fdc731e5f7f590f1
Lamin Samba <lamin.samba@angloamerican.com>
2018-10-19 22:36:26 +0100

    Add project files.

---
+++ Alpha.API/Startup.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.AzureAD.UI;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace Alpha.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.Audience = Configuration["AzureAd:ClientId"];
+                    options.Authority = $"{Configuration["AzureAd:Instance"]}{Configuration["AzureAd:TenantId"]}";
+                    options.SaveToken = true;
+                });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+                c.AddSecurityDefinition("oauth2", new OAuth2Scheme
+                {
+                    Type = "oauth2",
+                    Description = "OAuth2 Implicit Grant",
+                    Flow = "implicit",
+                    AuthorizationUrl = $"https://login.microsoftonline.com/{Configuration["AzureAD:TenantId"]}/oauth2/authorize",
+                    Scopes = new Dictionary<string, string>
+                    {
+                        { "user_impersonation", "Access API" }
+                    }
+                });
+                c.AddSecurityRequirement(new Dictionary<string, IEnumerable<string>>
+                {
+                    { "oauth2", new[] { "user_impersonation" } }
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.OAuthClientId(Configuration["AzureAD:ClientId"]);
+                c.OAuthClientSecret(Configuration["AzureAD:ClientSecret"]);
+                c.OAuthRealm(Configuration["AzureAD:Realm"]);
+                c.OAuthAppName("My API V1");
+                c.OAuthScopeSeparator(" ");
+                c.OAuthAdditionalQueryStringParams(new { resource = Configuration["AzureAD:ClientId"] });
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+        }
+    }
+}
